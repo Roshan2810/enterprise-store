984d31fbd198f06aa4de0fce26793bd04a1ec2ac
Roshan2810 <rosu285shah@gmail.com>
2021-08-23 15:36:22 +0530

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./ui/components/common/Header";
+import Carousel from "./ui/components/common/Carousel";
+import { Grid, Typography } from "@material-ui/core";
 
 function App() {
+  const products = [
+    { name: "Laptops", url: "" },
+    { name: "Tablets", url: "" },
+    { name: "Cameras", url: "" },
+  ];
+  const images = [
+    { url: "Iphone_11.jpg" },
+    { url: "Iphone_12.jpg" },
+    { url: "iphone.jpg" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Header />
+      <div style={{ margin: "2% 15%" }}>
+        <div style={{ height: "450px", border: "1px solid #00000029" }}>
+          <Carousel images={images} />
+        </div>
+        <Grid
+          container
+          spacing={2}
+          style={{ justifyContent: "center", marginTop: "2%" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+            style={{ textAlign: "center", marginBottom: "1%" }}
+          >
+            <Typography
+              variant="h5"
+              color="primary"
+              style={{ fontFamily: "inherit" }}
+            >
+              Trending Products
+            </Typography>
+          </Grid>
+          {products.map((product, i) => (
+            <Grid
+              key={i}
+              item
+              xs={12}
+              sm={12}
+              md={3}
+              lg={3}
+              xl={3}
+              style={{
+                border: "1px solid #00000029",
+                height: "200px",
+                margin: "0 1%",
+                padding: "4px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundImage: `url(${product.url})`,
+              }}
+            >
+              {product.name}
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
   );
 }
 
