09e8440f94dec0cff52232a4e3980cfd36addc7c
Roshan2810 <rosu285shah@gmail.com>
2021-08-23 15:34:38 +0530

    Basic structure

---
+++ src/ui/components/common/Carousel.jsx
@@ -0,0 +1,17 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import '../../../style/Carousel.css';
+
+function CommonCarousel(props) {
+  return (
+    <Carousel showArrows={true}>
+      {props.images.map((img, i) => (
+        <div key={i}>
+          <img src={img.url} alt={img.url}/>
+        </div>
+      ))}
+    </Carousel>
+  );
+}
+
+export default CommonCarousel;
---
+++ src/ui/components/common/Header.jsx
@@ -0,0 +1,55 @@
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  Avatar,
+  IconButton,
+} from "@material-ui/core";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+function Header() {
+  return (
+    <AppBar color="primary" position="static">
+      <Toolbar>
+        <Grid container>
+          <Grid
+            item
+            xs={6}
+            sm={6}
+            md={8}
+            lg={9}
+            xl={9}
+            style={{ display: "flex", alignItems: "center" }}
+          >
+            <Typography
+              variant="h5"
+              component="strong"
+              color="inherit"
+              style={{ fontFamily: "inherit" }}
+            >
+              Enterprise Store
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sm={6}
+            md={4}
+            lg={3}
+            xl={3}
+            style={{ display: "flex", alignItems: "center",justifyContent:'flex-end' }}
+          >
+            <IconButton style={{ marginLeft: "6px" }}>
+              <ShoppingCartIcon style={{ color: "white" }} fontSize="large" />
+            </IconButton>
+            <Typography variant="h6">David Warner</Typography>
+            <Avatar style={{ marginLeft: "15px" }}>DW</Avatar>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
